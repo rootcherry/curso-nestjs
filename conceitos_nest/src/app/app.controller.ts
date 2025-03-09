1a0a4ec79c38ee189a3edfaf50b50fe2028c846b
Chris N <hi.chrisn@gmail.com>
2025-03-09 17:04:25 -0300

    REST client

--- conceitos_nest/src/app/app.controller.ts
+++ conceitos_nest/src/app/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
 
-@Controller()
+@Controller('home') //home
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return 'Qualquer coisa.';
   }
 }
