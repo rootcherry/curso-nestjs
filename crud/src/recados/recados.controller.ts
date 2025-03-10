23a4ae5f816b09c8a83282af856af3f1c4386e69
Chris N <hi.chrisn@gmail.com>
2025-03-10 19:51:21 -0300

    HttpCode/HttpStatus

--- crud/src/recados/recados.controller.ts
+++ crud/src/recados/recados.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+  @HttpCode(HttpStatus.OK)
   @Get()
   findAll(): string {
     return 'Esta rota retorna todos os recados';
