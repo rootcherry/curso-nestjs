d4a6dee19218d7e8c3a3d038c3886a5362627285
Chris N <hi.chrisn@gmail.com>
2025-03-11 14:58:12 -0300

    service/entity

--- crud/src/recados/recados.service.ts
+++ crud/src/recados/recados.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Recado } from './entities/recado.entity';
 
 @Injectable()
 export class RecadosService {
+  private lastId = 1;
+  private recados: Recado[] = [
+    {
+      id: 1,
+      texto: 'Este é um recado de teste',
+      de: 'Joana',
+      para: 'João',
+      lido: false,
+      data: new Date(),
+    },
+  ];
+
   hello() {
     return 'Hello, World!';
   }
